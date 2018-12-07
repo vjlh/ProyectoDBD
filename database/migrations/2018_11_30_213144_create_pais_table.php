603b6c1c73f76a5d648d13880b95198013ff6677
Unknown <valentina.ligueno@gmail.com>
2018-12-07 17:38:44 -0300

    proyecto base
    bamos vien

---
+++ database/migrations/2018_11_30_213144_create_pais_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pais', function (Blueprint $table) {
+            $table->increments('id_pais');
+            $table->timestamps();
+            $table->string('nombre_pais');
+            $table->string('moneda_pais');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pais');
+    }
+}
