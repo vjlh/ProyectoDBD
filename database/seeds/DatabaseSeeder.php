6494a7f074d904444865c47366dc8009302a943f
Vale <valentina.ligueno@gmail.com>
2018-12-14 16:29:03 -0300

    DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(AdministradoresSeeders::class);
+        $this->call(AeropuertosSeeders::class);
+        $this->call(AsientosSeeders::class);
+        $this->call(AvionesSeeders::class);
+        $this->call(BeneficiosSeeders::class);
+        $this->call(CiudadesSeeders::class);
+        $this->call(EquipajesSeeders::class);
+        $this->call(HabitacionesSeeders::class);
+        $this->call(HistorialesSeeders::class);
+        $this->call(HospedajesSeeders::class);
+        $this->call(PaisesSeeders::class);
+        $this->call(PaquetesSeeders::class);
+        $this->call(PasajerosSeeders::class);
+        $this->call(PromocionesSeeders::class);
+        $this->call(ReservasSeeders::class);
+        $this->call(RestriccionesSeeders::class);
+        $this->call(SegurosSeeders::class);
+        $this->call(TicketsSeeders::class); 
+        $this->call(TransportesSeeders::class);
+        $this->call(UsersSeeders::class);
+        $this->call(VuelosSeeders::class);
+
     }
 }
