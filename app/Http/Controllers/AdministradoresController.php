9dd227d815f4790572e3864549abb0d003e3712a
Vale <valentina.ligueno@gmail.com>
2018-12-19 17:19:02 -0300

    Controlador Administrador funcionando
    Probé y funcionan todas las operaciones, las de create y edit no las hice, porque según mi compañero no eran por ahora necesarias, pero igual si pueden averigüen :D

--- app/Http/Controllers/AdministradoresController.php
+++ app/Http/Controllers/AdministradoresController.php
@@ -4,26 +4,41 @@ namespace App\Http\Controllers;
 
 use App\Administrador;
 use Illuminate\Http\Request;
+use Validator;
 
 class AdministradoresController extends Controller
 {
-
+    public function reglas(){
+        return[
+        'id_user' => 'required|numeric'
+        ];
+    }
     public function index()
     {
-        
+        return Administrador::all();
     }
 
     public function create()
     {
-        $administrador = Administrador::all();
-        return $administrador;
+        //
     }
 
     public function store(Request $request)
     {
-        $administrador = Administrador::create($request->all());
-        $administrador->save();
-        return "";
+        $validator = Validator::make($request->all(),$this->reglas());
+        if($validator->fails()){
+            return json_encode(['Datos Mal ingresados' => 'Error']);
+        }
+        $Administrador = new Administrador;
+        try{
+            $id = $request->get('id_user');
+            $Administrador->id_user = \App\User::find($id)->id;
+            $Administrador->save();
+            return $Administrador;
+        }
+        catch(\Exception $e){
+            return $e->getMessage();
+        }
     }
 
     public function show($id)
@@ -39,7 +54,20 @@ class AdministradoresController extends Controller
 
     public function update(Request $request, Administrador $administrador)
     {
-        //
+        $validator = Validator::make($request->all(),$this->reglas());
+        if($validator->fails()){
+            return json_encode(['Datos Mal ingresados' => 'Error']);
+        }
+
+        try{
+            $id = $request->get('id_user');
+            $administrador->id_user = \App\User::find($id)->id;
+            $administrador->save();
+            return $administrador;
+        }
+        catch(\Exception $e){
+            return $e->getMessage();
+        }
     }
 
     public function destroy($id)
--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
-use App\Vuelo;  
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,113 +17,26 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/vuelos', function () {
-    $vuelos = Vuelo::all();
-    return View('vuelos')->with('vuelos', $vuelos);
-});
-
-Route::get('/Administrador/all', 'AdministradoresController@index'); 
-Route::get('/Administrador/show/{id}', 'AdministradoresController@show'); 
-Route::post('/Administrador/destroy/{id}', 'AdministradoresController@destroy');
-Route::get('/Administrador/store', 'AdministradoresController@store');
-
-Route::get('/Aeropuerto/all', 'AeropuertosController@index'); 
-Route::get('/Aeropuerto/show/{id}', 'AeropuertosController@show'); 
-Route::post('/Aeropuerto/destroy/{id}', 'AeropuertosController@destroy');
-Route::get('/Aeropuerto/store', 'AeropuertosController@store');
-
-Route::get('/Asiento/all', 'AsientosController@index'); 
-Route::get('/Asiento/show/{id}', 'AsientosController@show'); 
-Route::post('/Asiento/destroy/{id}', 'AsientosController@destroy');
-Route::get('/Asiento/store', 'AsientosController@store');
-
-Route::get('/Avion/all', 'AvionesController@index'); 
-Route::get('/Avion/show/{id}', 'AvionesController@show'); 
-Route::post('/Avion/destroy/{id}', 'AvionesController@destroy');
-Route::get('/Avion/store', 'AvionesController@store');
-
-Route::get('/Beneficio/all', 'BeneficiosController@index'); 
-Route::get('/Beneficio/show/{id}', 'BeneficiosController@show'); 
-Route::post('/Beneficio/destroy/{id}', 'BeneficiosController@destroy');
-Route::get('/Beneficio/store', 'BeneficiosController@store');
-
-Route::get('/Ciudad/all', 'CiudadesController@index'); 
-Route::get('/Ciudad/show/{id}', 'CiudadesController@show'); 
-Route::post('/Ciudad/destroy/{id}', 'CiudadesController@destroy');
-Route::get('/Ciudad/store', 'CiudadesController@store');
-
-Route::get('/Equipaje/all', 'EquipajesController@index'); 
-Route::get('/Equipaje/show/{id}', 'EquipajesController@show'); 
-Route::post('/Equipaje/destroy/{id}', 'EquipajesController@destroy');
-Route::get('/Equipaje/store', 'EquipajesController@store');
-
-Route::get('/Habitacion/all', 'HabitacionesController@index'); 
-Route::get('/Habitacion/show/{id}', 'HabitacionesController@show'); 
-Route::post('/Habitacion/destroy/{id}', 'HabitacionesController@destroy');
-Route::get('/Habitacion/store', 'HabitacionesController@store');
-
-Route::get('/Historial/all', 'HistorialesController@index'); 
-Route::get('/Historial/show/{id}', 'HistorialesController@show'); 
-Route::post('/Historial/destroy/{id}', 'HistorialesController@destroy');
-Route::get('/Historial/store', 'HistorialesController@store');
-
-Route::get('/Hospedaje/all', 'HospedajesController@index'); 
-Route::get('/Hospedaje/show/{id}', 'HospedajesController@show'); 
-Route::post('/Hospedaje/destroy/{id}', 'HospedajesController@destroy');
-Route::get('/Hospedaje/store', 'HospedajesController@store');
-
-Route::get('/Pais/all', 'PaisesController@index'); 
-Route::get('/Pais/show/{id}', 'PaisesController@show'); 
-Route::post('/Pais/destroy/{id}', 'PaisesController@destroy');
-Route::get('/Pais/store', 'PaisesController@store');
-
-Route::get('/Paquete/all', 'PaquetesController@index'); 
-Route::get('/Paquete/show/{id}', 'PaquetesController@show'); 
-Route::post('/Paquete/destroy/{id}', 'PaquetesController@destroy');
-Route::get('/Paquete/store', 'PaquetesController@store');
-
-Route::get('/Pasajero/all', 'PasajerosController@index'); 
-Route::get('/Pasajero/show/{id}', 'PasajerosController@show'); 
-Route::post('/Pasajero/destroy/{id}', 'PasajerosController@destroy');
-Route::get('/Pasajero/store', 'PasajerosController@store');
-
-Route::get('/Promocion/all', 'PromocionesController@index'); 
-Route::get('/Promocion/show/{id}', 'PromocionesController@show'); 
-Route::post('/Promocion/destroy/{id}', 'PromocionesController@destroy');
-Route::get('/Promocion/store', 'PromocionesController@store');
-
-Route::get('/Reserva/all', 'ReservasController@index'); 
-Route::get('/Reserva/show/{id}', 'ReservasController@show'); 
-Route::post('/Reserva/destroy/{id}', 'ReservasController@destroy');
-Route::get('/Reserva/store', 'ReservasController@store');
-
-Route::get('/Restriccion/all', 'RestriccionesController@index'); 
-Route::get('/Restriccion/show/{id}', 'RestriccionesController@show'); 
-Route::post('/Restriccion/destroy/{id}', 'RestriccionesController@destroy');
-Route::get('/Restriccion/store', 'RestriccionesController@store');
-
-Route::get('/Seguro/all', 'SegurosController@index'); 
-Route::get('/Seguro/show/{id}', 'SegurosController@show'); 
-Route::post('/Seguro/destroy/{id}', 'SegurosController@destroy');
-Route::get('/Seguro/store', 'SegurosController@store');
-
-Route::get('/Ticket/all', 'TicketsController@index'); 
-Route::get('/Ticket/show/{id}', 'TicketsController@show'); 
-Route::post('/Ticket/destroy/{id}', 'TicketsController@destroy');
-Route::get('/Ticket/store', 'TicketsController@store');
-
-Route::get('/Transporte/all', 'TransportesController@index'); 
-Route::get('/Transporte/show/{id}', 'TransportesController@show'); 
-Route::post('/Transporte/destroy/{id}', 'TransportesController@destroy');
-Route::get('/Transporte/store', 'TransportesController@store');
-
-Route::get('/User/all', 'UsersController@index'); 
-Route::get('/User/show/{id}', 'UsersController@show'); 
-Route::post('/User/destroy/{id}', 'UsersController@destroy');
-Route::get('/User/store', 'UsersController@store');
-
-Route::get('/Vuelo/all', 'VuelosController@index'); 
-Route::get('/Vuelo/show/{id}', 'VuelosController@show'); 
-Route::post('/Vuelo/destroy/{id}', 'VuelosController@destroy');
-Route::get('/Vuelo/store', 'VuelosController@store');
+Route::resource('/Administrador','AdministradoresController');
+Route::resource('/Aeropuerto','AeropuertosController');
+Route::resource('/Asiento','AsientosController');
+Route::resource('/Avion','AvionesController');
+Route::resource('/Beneficio','BeneficiosController');
+Route::resource('/Ciudad','CiudadesController');
+Route::resource('/Equipaje', 'EquipajesController');
+Route::resource('/Habitacion', 'HabitacionesController');
+Route::resource('/Historial', 'HistorialesController');
+Route::resource('/Hospedaje','HospedajesController');
+Route::resource('/Pais','PaisesController');
+Route::resource('/Paquete','PaquetesController');
+Route::resource('/Pasajero','PasajerosController');
+Route::resource('/Promocion','PromocionesController');
+Route::resource('/Reserva','ReservasController');
+Route::resource('/Restriccion','RestriccionesController');
+Route::resource('/Seguro','SegurosController');
+Route::resource('/Ticket','TicketsController');
+Route::resource('/Transporte','TransportesController');
+Route::resource('/Seguro','SegurosController');
+Route::resource('/User','UsersController');
+Route::resource('/Seguro','SegurosController');
+Route::resource('/Vuelo','VuelosController');
