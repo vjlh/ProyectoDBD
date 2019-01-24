c7f1ee04b53cfc4d61dded4e3ec9e897d1ca2a19
Vale <valentina.ligueno@gmail.com>
2019-01-24 20:06:16 -0300

    Linea en rutas
    Esto aún no está probado aiuda, esto no es un meme

--- app/Http/Controllers/ReservasController.php
+++ app/Http/Controllers/ReservasController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Reserva;
+use App\Habitacion_Reserva;
 use Illuminate\Http\Request;
 use App\Http\Requests\ReservasRequest;
 
@@ -17,7 +18,36 @@ class ReservasController extends Controller
 
     public function create()
     {
-        //
+        $reserva = new Reserva;
+        $reserva->monto_total_reserva=request('total');
+        $reserva->check_in=request('check_in');
+        $reserva->id_user=request('id_user');
+        $reserva->id_seguro=request('id_seguro');
+        $reserva->id_promocion=request('id_promocion');
+        $reserva->id_paquete=request('id_paquete');
+        $reserva->transporte=true;
+        $reserva->hospedaje=true;
+        $reserva->vuelo=true;
+        
+    }
+    public function reservaHab()
+    {
+        $reserva = new Reserva;
+        $reserva->monto_total_reserva=request('total');
+        $reserva->check_in=null;
+        $reserva->id_user=request('id_user');
+        $reserva->id_seguro=null;
+        $reserva->id_promocion=null;
+        $reserva->id_paquete=null;
+        $reserva->transporte=false;
+        $reserva->hospedaje=false;
+        $reserva->vuelo=true;
+
+        $res_hab = new Habitacion_Reserva;
+        $res_hab->id_habitacion = request('id_habitacion');
+        $res_hab->id_reserva = $reserva->id;
+        $res_hab->fecha_inicio = "2019-10-10";
+        $res_hab->fecha_fin ="2019-10-10";
     }
 
     public function store(ReservasRequest $request)
