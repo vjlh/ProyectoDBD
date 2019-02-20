8f2d84fa60759650509553dfeccae77630e01b0f
Jose <jose.maureira.b@usach.cl>
2019-02-20 15:16:17 -0300

    cambios al front de paquetes
    toc

--- resources/views/seleccion_tipo_paquetes.blade.php
+++ resources/views/seleccion_tipo_paquetes.blade.php
@@ -6,15 +6,12 @@
 <form action="/paquetes" method="get">
 
 <section id="intro">
-<div class="carousel-background"><img src="{{asset('images/1.jpg')}}" alt=""></div>
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card dbd-auth" style="margin-top: -60%; color: white; background-color: #212529c7;">
-                <section id="about">
-      <div class="container">
+<style>#about::before {background: rgba(35, 32, 32, 0.92) }</style>
+<section id="about" >
+<div class="container" style="margin-top: 5%;">
 
         <header class="section-header">
-          <h3>Seleccione el tipo de paquete que desea</h3>
+          <h3 style="color: white;">Seleccione el tipo de paquete que desea</h3>
         </header>
 
         <div class="row about-cols">
@@ -27,8 +24,11 @@
               </div>
               <h2 class="title"><a href="/paquetes/Alojamiento">Vuelo + Alojamiento</a></h2>
               <p>
-                Aquí puedes encontrar todos los paquetes que involucran el vuelo hacia un maravilloso destino, así como también un excelente alojamiento con las mejores habitaciones para que puedas disfrutar una estadía inolvidable.
+                Aquí puedes encontrar todos los paquetes que involucran el vuelo hacia un maravilloso destino y un excelente alojamiento para que puedas disfrutar una estadía inolvidable.
               </p>
+              <center>
+              <a href="/paquetes/Alojamiento" class="btn btn-get-started scrollto">Seleccionar</a>
+              </center>
             </div>
           </div>
 
@@ -40,8 +40,11 @@
               </div>
               <h2 class="title"><a href="/paquetes/Automóvil">Vuelo + Automóvil</a></h2>
               <p>
-                Aquí puedes encontrar todos los paquetes que involucran el vuelo hacia un maravilloso destino, así como también un vehículo que te permitirá desplazarte a donde sea que desees ir.
+                Aquí puedes encontrar todos los paquetes que involucran el vuelo hacia un maravilloso destino y también un vehículo que te permitirá desplazarte a donde sea que desees ir.
               </p>
+              <center>
+              <a  href="/paquetes/Automóvil" class="btn btn-get-started scrollto">Seleccionar</a>
+              </center>
             </div>
           </div>
             </div>
