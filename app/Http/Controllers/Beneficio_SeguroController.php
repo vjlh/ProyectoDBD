7cf9f4221c635dcd4cb8398383010e4800b222f4
Vale <valentina.ligueno@gmail.com>
2019-02-26 16:54:19 -0300

    Seguro al 90%
    Falta la blade final para mostrar resumen

--- app/Http/Controllers/Beneficio_SeguroController.php
+++ app/Http/Controllers/Beneficio_SeguroController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\Beneficio_SeguroRequest;
 use App\Beneficio_Seguro;
+use App\Reserva;
+use App\Seguro;
 
 
 class Beneficio_SeguroController extends Controller
@@ -81,5 +83,46 @@ class Beneficio_SeguroController extends Controller
         else
             return "No existe un bs con la id ingresada";
     }
-    
+    public function adquirirSeguro()
+    {
+        $seguro = new Seguro;
+        $reserva = new Reserva;
+
+        $duracion_seguro = session()->get('diasDuracion_seguro');
+        $inicio_seguro = session()->get('fechaInicio_seguro');
+        $fin_seguro = session()->get('fechaFin_seguro');
+        $destino_seguro = session()->get('destino_seguro');
+        $personas_seguro = session()->get('numeroPasajeros_seguro');
+        $costo_seguro = session()->get('costoFinalGrupal_seguro');
+        $beneficios_seguro = session()->get('beneficiosSeleccionados_seguro');
+
+        $seguro->precio_seguro = $costo_seguro;
+        $seguro->tipo_seguro = "algo";
+        $seguro->destino_seguro = $destino_seguro;
+        $seguro->numero_pasajeros_seguros = $personas_seguro;
+        $seguro->fecha_inicio_seguro = $inicio_seguro;
+        $seguro->fecha_fin_seguro = $fin_seguro;
+        $seguro->save();
+
+        
+        foreach($beneficios_seguro as $ben){
+            $beneficio_seguro = new Beneficio_Seguro;
+            $beneficio_seguro->id_seguro = $seguro->id;
+            $beneficio_seguro->id_beneficio = $ben->id;
+            $beneficio_seguro->save();
+        }
+
+        $reserva->monto_total_reserva=$costo_seguro;
+        $reserva->check_in=null;
+        $reserva->id_user=auth()->id();
+        $reserva->id_seguro=$seguro->id;
+        $reserva->id_promocion=null;
+        $reserva->id_paquete=null;
+        $reserva->transporte=false;
+        $reserva->hospedaje=false;
+        $reserva->vuelo=false;
+        $reserva->save();
+        return view('home');
+
+    }
 }
